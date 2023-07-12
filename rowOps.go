fbdc6a133fb8390038be04eb4493de7cdbe01afc
Jianfeng Li <jianfeng.li@pwc.com>
2023-07-12 14:07:46 +0930

    Add biop function targeting two elements of a slice

--- rowOps.go
+++ rowOps.go
@@ -15,3 +15,8 @@ func replace(row []string, markers []int, operator func([]string)) {
 		row[m] = working[i]
 	}
 }
+
+// biop operates on two elements of a slice marked by indxA and indxB, returns a new slice with original elements and one more of the operational result
+func biop(row []string, indxA, indxB int, operator func(a, b string) string) []string {
+	return append(row, operator(row[indxA], row[indxB]))
+}
--- rowOps_test.go
+++ rowOps_test.go
@@ -31,3 +31,20 @@ func TestReplace(t *testing.T) {
 		}
 	}
 }
+
+func TestBiOp(t *testing.T) {
+	original := []string{"a", "b", "c"}
+
+	concat := func(a, b string) string {
+		return a + b
+	}
+	result := biop(original, 0, 2, concat)
+
+	if len(original)+1 != len(result) {
+		t.Error("Expecting only increasing by one, got ", len(result)-len(original))
+	}
+
+	if result[len(result)-1] != original[0]+original[2] {
+		t.Errorf("Expecting the last element to be %s, but got %s\n", original[0]+original[2], result[len(result)-1])
+	}
+}
